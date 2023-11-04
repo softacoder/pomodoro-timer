4cab19329f8522c45b9993ef8a716ff460ad83c4
softacoder <113185323+softacoder@users.noreply.github.com>
2023-11-04 20:33:03 +0000

    commit

--- background.js
+++ background.js
@@ -2,15 +2,28 @@ chrome.alarms.create("pomodoroTimer", {
   periodInMinutes: 1 / 60,
 });
 
-chrome.alarms.onAlarm.addListner((alarm) => {
+chrome.alarms.onAlarm.addListener((alarm) => {
   if (alarm.name === "pomodoroTimer") {
     chrome.storage.local.get(["timer, isRunning"], (res) => {
       if (res.isRunning) {
         let timer = res.timer + 1;
+        let isRunning = true;
+        if (timer === 60 * 25) {
+          this.registration.showNotification("Pomodoro Timer", {
+            body: "25 minutes has passed!",
+            icon: "icon.png",
+          });
+        }
         chrome.storage.local.set({
           timer,
         });
+        timer = 0;
+        isRunning = false;
       }
+      chrome.storage.local.set({
+        timer,
+        isRunning,
+      });
     });
   }
 });
